a425029b594432797efa80713aba8a2723ed0c45
Guilherme Gomes <guilherme2050@gmail.com>
2024-03-14 14:29:39 -0300

    fix: corrigindo models e atualizando gitignore

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 class Tasks(models.Model):
-    STATUS = (
-        ('todo', 'To Do'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done'),
+
+    STATUS_CHOICES = (
+        ('to_do', 'A fazer'),
+        ('in_progress', 'Em progresso'),
+        ('done', 'Concluído'),
     )
+
+    PRIORITY_CHOICES = (
+        ('urgent', 'Urgente'),
+        ('high', 'Alta'),
+        ('medium', 'Média'),
+        ('low', 'Baixa'),
+    )
+
     title = models.CharField(max_length=100)
     description = models.TextField(null=True)
-    status = models.CharField(max_length=12, choices=STATUS, default='fazendo')
-    priority = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add_date=True)
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='Em progresso')
+    priority = models.CharField(max_length=12, choices=PRIORITY_CHOICES, default='Média')
+    created_at = models.DateTimeField(auto_now_add=True)
