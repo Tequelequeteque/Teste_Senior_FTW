b323cb5f1517ffce4dc6f582ed9a7c1659438d96
Guilherme Gomes <guilherme2050@gmail.com>
2024-03-14 14:56:44 -0300

    feature: implementando ordenação e filtro pelos campos ´status´,´priority´, ´created_at´ e correção das rotas

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from .serializers import TaskSerializer
 from .models import Tasks
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Tasks.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['status','priority','created_at']
+    search_fields = ['status','priority','created_at']
 
